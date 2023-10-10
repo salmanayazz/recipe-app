bfb8eb4bf68a473b2439864933fd38dddd12a762
Salman Ayaz <salmanayazz@gmail.com>
2023-10-10 01:28:18 -0700

    simplified db queries and added search params

--- server/src/models/Recipe.js
+++ server/src/models/Recipe.js
@@ -10,8 +10,7 @@ var recipeSchema = new mongoose.Schema({
         required: true,
     },
     ingredients: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Ingredient',
+        type: [String],
         required: true,
     }],
     directions: {
