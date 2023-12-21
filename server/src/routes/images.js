a0858e4fdaf07648af13bd0396de2f4c4fd2e84c
Salman Ayaz <salmanayazz@gmail.com>
2023-12-20 22:00:28 -0800

    fixed image get method

--- server/src/routes/images.js
+++ server/src/routes/images.js
@@ -4,6 +4,6 @@ const { getImage, uploadImage, deleteImage } = require('../controllers/images')
 
 const router = express.Router();
 
-router.post('/', uploadImage);
+router.get('/:imageName', getImage);
 
 module.exports = router;
