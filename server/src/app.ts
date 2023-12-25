5c5969ad3bba9eeee1c3fb53df98c9c0a2531510
Salman Ayaz <salmanayazz@gmail.com>
2023-12-24 22:54:36 -0800

    removed views from server

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,6 @@ import mongoose from "mongoose";
 import session from "express-session";
 import dotenv from "dotenv";
 
-import indexRouter from "./routes/index";
 import authRouter from "./routes/auth";
 import recipesRouter from "./routes/recipes";
 import imagesRouter from "./routes/images";
@@ -18,10 +17,6 @@ dotenv.config();
 const app = express();
 
 export default app;
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -63,25 +58,12 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
 app.use("/auth", authRouter);
 app.use("/recipes", recipesRouter);
 app.use("/images", imagesRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
+  res.status(404).send("Not found");
 });
 
-// error handler
-app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+export { app };
--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, NextFunction, Response } from "express";
-const router = express.Router();
-
-/* GET home page. */
-router.get("/", function (req: Request, res: Response, next: NextFunction) {
-  res.render("index", { title: "Express" });
-});
-
-export default router;
