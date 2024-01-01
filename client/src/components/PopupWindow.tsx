8cd62d5b4614644dd4e600d22232f51ef2d85187
Salman Ayaz <salmanayazz@gmail.com>
2023-12-31 21:49:13 -0800

    set close button to stay in place on PopupWindow

--- client/src/components/PopupWindow.tsx
+++ client/src/components/PopupWindow.tsx
@@ -42,11 +42,12 @@ export default function PopupWindow({
       }}
     >
       <div
-        className={`flex flex-col bg-pri-100 z-10 absolute top-1/2 left-1/2 transform
-                -translate-x-1/2 -translate-y-1/2 px-8 py-5 overflow-x-none rounded-md overflow-y-auto max-h-[90%]`}
+        className={`flex flex-col bg-pri-100 z-10 absolute top-1/2 left-1/2 -translate-x-1/2 transform -translate-y-1/2 rounded-md max-h-[90%]`}
         style={{ width: sizeToStyle }}
       >
-        {element}
+        <div className="overflow-y-auto overflow-x-hidden py-5 px-8">
+          {element}
+        </div>
 
         <div className="flex absolute top-3 right-3">
           <CloseButton onClick={onExit} />
